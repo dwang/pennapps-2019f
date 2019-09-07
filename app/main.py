9ae5860efc637c7cdb654fc411feb56295236ad0
Daniel Wang <hello@danielwang.dev>
2019-09-07 11:32:32 -0400

    feat: refactor website

--- app/main.py
+++ app/main.py
@@ -12,9 +12,11 @@ def hello_world():
 def query_dataset(ticker):
     return marquee.query_dataset(ticker)
 
+
 @app.route("/api/marquee/asset/<ticker>")
 def get_asset(ticker):
     return marquee.get_asset(ticker)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
