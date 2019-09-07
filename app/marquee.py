0a154dc48b80ecd7b8f17773dda5b813e04f5808
Daniel Wang <hello@danielwang.dev>
2019-09-07 01:06:38 -0400

    feat: dump marquee api company names

--- app/marquee.py
+++ app/marquee.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 auth_data = {
     "grant_type": "client_credentials",
@@ -30,7 +33,44 @@ def get_marquee_data(ticker):
         "endDate": "2018-01-15"
     }
 
-    request = session.post(url=request_url, json=request_query)
+    request = session.post(
+        url="https://api.marquee.gs.com/v1/data/USCANFPP_MINI/query", json=request_query)
     results = json.loads(request.text)
 
     return results
+
+
+def get_marquee_tickers():
+    session = requests.Session()
+
+    auth_request = session.post(
+        "https://idfs.gs.com/as/token.oauth2", data=auth_data)
+    access_token_dict = json.loads(auth_request.text)
+    access_token = access_token_dict["access_token"]
+
+    session.headers.update({"Authorization": "Bearer " + access_token})
+
+    coverage = session.get(
+        url="https://api.marquee.gs.com/v1/data/USCANFPP_MINI/coverage?limit=200")
+    coverage_data = json.loads(coverage.text)
+
+    reference_query = {
+        "where": {
+            "gsid": [point["gsid"] for point in coverage_data["results"]]
+        },
+        "fields": ["gsid", "ticker", "name"],
+        "limit": 200
+    }
+
+    reference = session.post(
+        url="https://api.marquee.gs.com/v1/assets/data/query", json=reference_query)
+    reference_data = json.loads(reference.text)
+    companies_set = {(result["ticker"], result["name"])
+                     for result in reference_data["results"]}
+
+    with open("static/companies.json", "w") as out:
+        json.dump(list(companies_set), out)
+
+
+if __name__ == "__main__":
+    get_marquee_tickers()
