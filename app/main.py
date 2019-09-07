93a0bee2dbd2ac1ace66730b1efadc9f3d081568
Daniel Wang <hello@danielwang.dev>
2019-09-07 01:08:39 -0400

    style: format main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
+from marquee import get_marquee_data
 from flask import Flask
 app = Flask(__name__)
 
-from dotenv import load_dotenv
-load_dotenv()
-
-from marquee import get_marquee_data
 
 @app.route("/")
 def hello_world():
     return "Hello World from Flask"
 
+
 @app.route("/api/marquee/<ticker>")
 def marquee_data(ticker):
     return get_marquee_data(ticker)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
