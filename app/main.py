560e76e90eb5ce28b35d798be79b6a55c376c3cc
Daniel Wang <hello@danielwang.dev>
2019-09-08 04:39:10 -0400

    feat: finish autocomplete and add charts

--- app/main.py
+++ app/main.py
@@ -12,9 +12,20 @@ def home():
 @app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
     if request.method == "POST":
-        ticker = request.form.get("ticker")
+        ticker = request.form.get("ticker").split("-")[0].strip()
         data = marquee.query_dataset(ticker)
-        return render_template("dashboard.html", ticker=ticker, data=data)
+        integrated_factor = []
+        growth = []
+        financial_returns = []
+        multiple = []
+
+        for i in range(120):
+            growth.append(data["data"][i * int(len(data["data"]) / 120) - 1]["growthScore"])
+            financial_returns.append(data["data"][i * int(len(data["data"]) / 120) - 1]["financialReturnsScore"])
+            multiple.append(data["data"][i * int(len(data["data"]) / 120) - 1]["multipleScore"])
+            integrated_factor.append(data["data"][i * int(len(data["data"]) / 120) - 1]["integratedScore"])
+
+        return render_template("dashboard.html", ticker=ticker, integrated_factor=integrated_factor, growth=growth, financial_returns=financial_returns, multiple=multiple)
 
     return redirect(url_for("home"))
 
