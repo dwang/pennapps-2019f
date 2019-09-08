c52addb5f951661add35fc5d5acb21102548b017
Jebbly <jeffliu604@gmail.com>
2019-09-07 23:59:08 -0400

    feat: added alphavantage api

---
+++ app/alphavantage.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def query_prices(ticker, date):
+    payload = {'function': 'TIME_SERIES_DAILY', 'symbol': ticker, 'outputsize': 'full', 'apikey': os.getenv("ALPHA_VANTAGE_API_KEY")}
+    r = requests.get('https://www.alphavantage.co/query', params=payload)
+
+    time_series = r.json()["Time Series (Daily)"]
+
+    return time_series[date]["4. close"]
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import marquee
+import alphavantage
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -16,10 +17,14 @@ def hello():
 def query_dataset(ticker):
     return marquee.query_dataset(ticker)
 
-
 @app.route("/api/marquee/asset/<ticker>")
 def get_asset(ticker):
     return marquee.get_asset(ticker)
+
+
+@app.route("/api/alphavantage/query/<ticker>/<date>")
+def query_prices(ticker, date):
+    return alphavantage.query_prices(ticker, date)
 
 
 if __name__ == "__main__":
